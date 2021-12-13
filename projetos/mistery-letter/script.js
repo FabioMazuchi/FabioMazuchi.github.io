cb3f0e5477c8f6c227a3b8c03b5003f685cecbd4
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2021-12-13 13:38:16 -0300

    altera mystery letter

--- projetos/mistery-letter/script.js
+++ projetos/mistery-letter/script.js
@@ -5,20 +5,28 @@ const estilo = ['newspaper', 'magazine1', 'magazine2'];
 const tamanho = ['medium', 'big', 'reallybig'];
 const rotacao = ['rotateleft', 'rotateright'];
 const iniciacao = ['skewleft', 'skewright'];
+const pContador = document.querySelector('#carta-contador');
+
+function contaPalavras() {
+  let cont = 0;
+  let array = input.value.trim();
+  array = array.split(' ');
+  for (let i = 0; i < array.length; i += 1) {
+    if (array[i] !== '' && array[i].length >= 1) {
+      cont += 1;
+    }
+    pContador.innerHTML = cont;
+  }
+}
 
 function adicionaClasseEstilo(span, array) {
-  const aleatorio = array[Math.round(Math.random() * array.length)];
+  const aleatorio = array[Math.trunc(Math.random() * array.length)];
   span.classList.add(aleatorio);
 }
 
-function adicionaSpan() {
-  const array = input.value.split(' ');
-  p.innerHTML = '';
-
-  if (input.value === '' || input.value === ' ') {
-    p.innerHTML = 'Por favor, digite o conteúdo da carta.';
-  } else {
-    for (let a = 0; a < array.length; a += 1) {
+const addAllClass = (array) => {
+  for (let a = 0; a < array.length; a += 1) {
+    if (array[a] !== ' ') {
       const span = document.createElement('span');
       span.innerHTML = array[a];
       adicionaClasseEstilo(span, estilo);
@@ -28,6 +36,18 @@ function adicionaSpan() {
       p.appendChild(span);
     }
   }
+};
+
+function adicionaSpan() {
+  let array = input.value.trim();
+  array = array.split(' ');
+  p.innerHTML = '';
+  if (input.value === '' || input.value === ' ') {
+    p.innerHTML = 'Por favor, digite o conteúdo da carta.';
+  } else {
+    addAllClass(array);
+  }
 }
 
 btn.addEventListener('click', adicionaSpan);
+btn.addEventListener('click', contaPalavras);
