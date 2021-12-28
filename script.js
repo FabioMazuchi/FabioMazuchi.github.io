5f01b5cded919a5fef6de1cafd3c6e1f2f0099ee
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2021-12-28 19:10:39 -0300

    altera estilos

--- script.js
+++ script.js
@@ -4,25 +4,45 @@ const btnSkills = document.querySelector("#skills");
 const divInfo = document.querySelector(".sobre");
 const divProjetos = document.querySelector(".projetos");
 const divSkills = document.querySelector(".skills");
+const body = document.getElementsByTagName('body');
+console.log(body[0]);
+
+const trocaFundoSobre = () => {
+	body[0].style.backgroundImage = 'url(../images/sobre.png)'
+}
+
+
+const trocaFundoProjetos = () => {
+	body[0].style.backgroundImage = 'url(../images/projetos.jpg)'
+}
+
+const trocaFundoSkills = () => {
+	body[0].style.backgroundImage = 'url(../images/skills.jpg)'
+}
 
 const addSkills = () => {
 	divSkills.style.display = 'block';
 	divProjetos.style.display = 'none';
 	divInfo.style.display = 'none';
+	trocaFundoSkills();
 };
 
 const addInfo = () => {
 	divInfo.style.display = 'block';
 	divProjetos.style.display = 'none';
 	divSkills.style.display = 'none';
+	trocaFundoSobre();
 };
 
 const addProjeto = () => {
 	divInfo.style.display = 'none';
 	divProjetos.style.display = 'block';
 	divSkills.style.display = 'none';
+	trocaFundoProjetos();
 };
 
 btnSkills.addEventListener('click', addSkills);
 btnProjetos.addEventListener("click", addProjeto);
 btnSobre.addEventListener('click', addInfo);
+
+window.onload = trocaFundoSobre;
