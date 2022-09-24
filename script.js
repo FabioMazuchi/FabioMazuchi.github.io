d9a3c0df092d026193d1bbe19fa6a424f5b7f38a
Fabio Augusto Mazuchi <mazuchi.augusto@hotmail.com>
2022-09-24 11:28:22 -0300

    update

--- script.js
+++ script.js
@@ -2,32 +2,16 @@ const btnProjetos = document.querySelector("#projetos");
 const btnSobre = document.querySelector("#sobre");
 const divInfo = document.querySelector(".sobre");
 const divProjetos = document.querySelector(".projetos");
-const overlay = document.querySelector(".overlay");
-const body = document.getElementsByTagName('body');
-
-const trocaFundoSobre = () => {
-	body[0].style.backgroundImage = 'url(../images/azulBranco.webp)'	
-}
-
-
-const trocaFundoProjetos = () => {
-	body[0].style.backgroundImage = 'url(../images/projetos.jpg)'
-	overlay.style.display = 'block';
-}
 
 const addInfo = () => {
 	divInfo.style.display = 'flex';
 	divProjetos.style.display = 'none';
-	trocaFundoSobre();
 };
 
 const addProjeto = () => {
 	divInfo.style.display = 'none';
 	divProjetos.style.display = 'block';
-	trocaFundoProjetos();
 };
 
 btnProjetos.addEventListener("click", addProjeto);
 btnSobre.addEventListener('click', addInfo);
-
-window.onload = trocaFundoSobre;
